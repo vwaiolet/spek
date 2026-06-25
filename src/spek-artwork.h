294e55c7715ecce38478c289baf69ad5921187b6
vwaiolet <passwordk@kakao.com>
2026-06-26 01:09:15 +0900

    Remove legacy build and packaging

--- src/spek-artwork.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define ART_SPEK "art-spek"
-#define ART_OPEN "art-open"
-#define ART_SAVE "art-save"
-#define ART_HELP "art-help"
-#define ART_CLOSE "art-close"
-
-void spek_artwork_init();
